a40c66cde8dba1601365ff60794bf2fe748434e1
shaheer-haider <shaheerhaider00000@gmail.com>
2023-10-26 20:13:34 +0500

    .nuxt removed

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/home/shaheer/Desktop/amazon-commerce/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default /* #__PURE__ */ defuFn(inlineConfig)
--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///home/shaheer/Desktop/amazon-commerce/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
